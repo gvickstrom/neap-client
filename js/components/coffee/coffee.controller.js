56566ea55867490898eade5a78588b7eada6ccba
gvickstrom <gvickstrom@gmail.com>
2016-10-24 15:28:36 -0600

    push to github pages

---
+++ js/components/coffee/coffee.controller.js
@@ -0,0 +1,53 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('myApp.components.coffee', [])
+    .controller('coffeeController', coffeeController);
+
+  coffeeController.$inject = ['$scope', 'coffeeService'];
+
+  function coffeeController($scope, coffeeService) {
+    /*jshint validthis: true */
+    const vm = this;
+    vm.form = false;
+    vm.coffeeObj = init();
+    vm.showForm = function() {
+      vm.form = true;
+    };
+    vm.addCoffee = function() {
+      coffeeService.addCoffee(vm.coffeeObj);
+      vm.coffeeObj = init();
+    };
+    coffeeService.getAllCoffee()
+    .then((coffees) => {
+      vm.coffee = coffees.data.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    // coffeeService.getSingleCoffee(1)
+    // .then((coffee) => {
+    //   console.log(coffee);
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+  }
+
+  function init() {
+    const coffeeObj = {
+      name: 'The Good Stuff',
+      roaster: 'Sweet Bloom',
+      origin: 'Derek\'s Backyard',
+      roast: 'Medium',
+      caffeine: 100,
+      decaf: 'true',
+      price: 6,
+      quantity: 1
+    };
+    return coffeeObj;
+  }
+
+})();
